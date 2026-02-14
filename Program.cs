5833be5b5d122a2dabf99b44dc895f6d230c4445
Dmitriy Logunov <logunovdm@gmail.com>
2026-02-14 11:18:35 +1100

    Initial commit: TicTacToe ASP.NET project

---
+++ Program.cs
@@ -0,0 +1,9 @@
+// Program.cs
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.MapHub<GameHub>("/gamehub");
+app.Run();
